530386ba38f62c41520c06395e7c810de64b5bfc
keryi <hti_32@yahoo.com>
2015-07-09 15:39:16 +0000

    add submit post feature

--- bloog/app/controllers/application_controller.rb
+++ bloog/app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :init_blog
+
+  private
+
+  def init_blog
+    @blog = THE_BLOG
+  end
 end
