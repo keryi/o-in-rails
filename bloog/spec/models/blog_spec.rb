58cdb3d7fce769d811699bad1f03e2c24fcffb73
keryi <hti_32@yahoo.com>
2015-07-13 15:36:53 +0000

    add sorted blog entries with limit 10

--- bloog/spec/models/blog_spec.rb
+++ bloog/spec/models/blog_spec.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'ostruct'
 require_relative '../../app/models/blog'
+require 'date'
 
 describe Blog do
   before { @blog = Blog.new }
@@ -34,9 +35,34 @@ describe Blog do
 
   describe '#add_entry' do
     it 'add entries to the blog' do
-      entry = Object.new
+      entry = OpenStruct.new(pubdate: Date.today)
       @blog.add_entry entry
       @blog.entries.must_include entry
+    end
+  end
+
+  describe '#entries' do
+    def stub_entry_with_date(date)
+      OpenStruct.new(pubdate: DateTime.parse(date))
+    end
+
+    it 'is sorted with reverse-chronological-order' do
+      oldest = stub_entry_with_date('2011-09-09')
+      newest = stub_entry_with_date('2011-09-11')
+      middle = stub_entry_with_date('2011-09-10')
+
+      @blog.add_entry oldest
+      @blog.add_entry newest
+      @blog.add_entry middle
+      @blog.entries.must_equal [newest, middle, oldest]
+    end
+
+    it 'is limited to 10 items' do
+      10.times { |i| @blog.add_entry(stub_entry_with_date("2011-09-#{i + 1}")) }
+      oldest = stub_entry_with_date('2011-08-30')
+      @blog.add_entry oldest
+      @blog.entries.size.must_equal 10
+      @blog.entries.wont_include oldest
     end
   end
 end
